e99f9707c9ba483dedf112ca96fd68af8ca40348
cstone-io <connor@stoneconsulting.io>
2023-12-03 05:59:55 -0500

    corrected script

--- chroma/script.py
+++ chroma/script.py
@@ -1,25 +1,30 @@
 import chromadb
+from chromadb.config import Settings
 import yaml
 
-from ..src.utils.config import Config
+HOST = "localhost"
+PORT = "8000"
 
 
 def main():
-    config = Config.get()
-    chroma_kwargs = config.chromadb.to_dict()
-    chroma = chromadb.HttpClient(**chroma_kwargs)
+    chroma = chromadb.HttpClient(
+        host=HOST,
+        port=PORT,
+        settings=Settings(chroma_api_impl="chromadb.api.fastapi.FastAPI"),
+    )
 
+    chroma.delete_collection("main")
     collection = chroma.create_collection("main")
 
     with open("seed.yaml", "r") as f:
         context = yaml.load(f, Loader=yaml.SafeLoader)
 
     collection.add(
-        documents=[document["body"] for document in context],
+        documents=[context[document]["body"] for document in context],
         metadatas=[
             dict(
-                title=document["title"],
-                source=document["link"],
+                title=context[document]["title"],
+                source=context[document]["link"],
             )
             for document in context
         ],
